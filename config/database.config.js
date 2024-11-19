1a585576c3aad94e3a28c76aef6274c2be41dbce
iPrazel <phnghiaa23031@cusc.ctu.edu.vn>
2024-11-19 08:24:19 +0700

    second commit

--- config/database.config.js
+++ config/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/users');
+        await mongoose.connect('mongodb+srv://phnghiaa23031:huunghia2410@cluster0.a7sg6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('Kết nối MongoDB thành công');
     } catch (err) {
         console.error('Lỗi kết nối MongoDB:', err);
